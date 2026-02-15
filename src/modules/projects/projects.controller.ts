b734a38b2fbb6f32a6d5cb60369b8b2d68e42980
Bruno_Neves <nevesbruno814@gmail.com>
2026-02-14 21:52:27 -0400

    creating the methods

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common'
+import { ProjectsService } from './projects.service'
 
-@Controller('projects')
-export class ProjectsController {}
+@Controller({ version: '1', path: 'projects' })
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Get()
+  findAll() {
+    return this.projectsService.findAll()
+  }
+
+  @Get(':id')
+  findById(id: string) {
+    return this.projectsService.findById(id)
+  }
+
+  @Post()
+  create(data: any) {
+    return this.projectsService.create(data)
+  }
+
+  @Put(':id')
+  update(id: string, data: any) {
+    return this.projectsService.update(id, data)
+  }
+
+  @Delete(':id')
+  delete(id: string) {
+    return this.projectsService.delete(id)
+  }
+}
--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  findAll() {
+    return ['Projeto 1', 'Projeto 2', 'Projeto 3']
+  }
+
+  findById(id: string) {
+    return `Projeto ${id}`
+  }
+
+  create(data: any) {
+    return 'Projeto criado com sucesso!'
+  }
+
+  update(id: string, data: any) {
+    return `Projeto ${id} atualizado com sucesso!`
+  }
+
+  delete(id: string) {
+    return `Projeto ${id} deletado com sucesso!`
+  }
+}
