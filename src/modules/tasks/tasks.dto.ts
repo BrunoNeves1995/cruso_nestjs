8b2e01ed751dc4ec7b8199cb7d22c634dc619a7b
Bruno_Neves <nevesbruno814@gmail.com>
2026-02-15 16:20:20 -0400

    config ApiProperty (sqagger) e class-validator

--- src/modules/tasks/tasks.dto.ts
+++ src/modules/tasks/tasks.dto.ts
@@ -1,9 +1,41 @@
+import { Optional } from "@nestjs/common";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDateString, IsEnum, IsNotEmpty, isNotEmpty, IsOptional, IsString } from "class-validator";
 import { TaskPriority, TaskStatus } from "src/generated/prisma/enums";
 
 export class TaskDTO{
+  @ApiProperty({description: "Task titule"})
+  @IsString()
+  @IsNotEmpty()
   title!: string;
+
+  @ApiProperty({description: "Task description", required: false})
+  @IsString()
+  @IsOptional()
   description!: string;
+
+  @ApiProperty({
+    description: "Task status",
+    enum: TaskStatus,
+    default: TaskStatus.TODO,
+    required: false
+  })
+  @IsEnum(TaskStatus)
+  @IsOptional()
   status?: TaskStatus = TaskStatus.TODO;
+
+  @ApiProperty({
+    description: "Task priority",
+    enum: TaskPriority,
+    default: TaskPriority.MEDIUM,
+    required: false
+  })
+  @IsEnum(TaskPriority)
+  @IsOptional()
   priority?: TaskPriority = TaskPriority.MEDIUM;
+
+  @ApiProperty({description: "Task dueDate"})
+  @IsDateString()
+  @IsOptional()
   dueDate?: string;
 }
