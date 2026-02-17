0692f8e9f410216d70e0aba947068348f324eac3
Bruno Neves <brunoneves814@gmail.com>
2026-02-17 14:57:53 -0400

    implementing methods basic of the user in class of service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,4 +4,47 @@ import { PrismaService } from '../prisma/prisma.service'
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+  }
+
+  async findAll() {
+    return await this.prisma.user.findMany()
+  }
+
+  async create(data: any) {
+    return await this.prisma.user.create({
+      data,
+    })
+  }
+
+  async update(id: string, data: any) {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+    })
+  }
+
+  async delete(id: string) {
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    })
+  }
 }
