3f27202d2a5b8ea4f47e34bfdfad27c43b926845
Bruno Neves <brunoneves814@gmail.com>
2026-02-17 15:39:06 -0400

    implementing methods of class userController

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common'
+import { UsersService } from './users.service'
 
-@Controller('users')
-export class UsersController {}
+@Controller({ version: '1', path: 'users' })
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll() {
+    return await this.usersService.findAll()
+  }
+
+  @Get(':userId')
+  async findById(@Param('userId', ParseUUIDPipe) userId: string) {
+    const user = await this.usersService.findById(userId)
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado')
+    }
+    return user
+  }
+
+  @Get(':email')
+  async findByEmail(@Param('email') email: string) {
+    const user = await this.usersService.findByEmail(email)
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado')
+    }
+    return user
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() data: any) {
+    return await this.usersService.create(data)
+  }
+
+  @Put(':userId')
+  @HttpCode(HttpStatus.OK)
+  async update(@Param('userId', ParseUUIDPipe) userId: string, @Body() data: any) {
+    const user = await this.usersService.findById(userId)
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado')
+    }
+    return await this.usersService.update(userId, data)
+  }
+
+  @Delete(':userId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('userId', ParseUUIDPipe) userId: string) {
+    const user = await this.usersService.findById(userId)
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado')
+    }
+    return await this.usersService.delete(userId)
+  }
+}
