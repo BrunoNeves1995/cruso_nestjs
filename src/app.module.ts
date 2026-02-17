e3fe19ff1cb9ad48e93f573fa82a3fb6ff242394
Bruno Neves <brunoneves814@gmail.com>
2026-02-17 14:44:57 -0400

    cretinf modules users

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { TasksController } from './modules/tasks/tasks.controller';
 import { TasksService } from './modules/tasks/tasks.service';
 import { TasksModule } from './modules/tasks/tasks.module';
+import { UsersController } from './modules/users/users.controller';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ProjectsModule, PrismaModule, TasksModule],
-  controllers: [AppController],
+  imports: [ProjectsModule, PrismaModule, TasksModule, UsersModule],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
