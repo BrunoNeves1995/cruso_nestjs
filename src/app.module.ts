0958c2b1cbdeb696991b24327fb857581be52804
Bruno Neves <brunoneves814@gmail.com>
2026-02-17 15:38:08 -0400

    ajusting in imports with errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ProjectsModule } from './modules/projects/projects.module';
-import { PrismaModule } from './modules/prisma/prisma.module';
-import { TasksController } from './modules/tasks/tasks.controller';
-import { TasksService } from './modules/tasks/tasks.service';
-import { TasksModule } from './modules/tasks/tasks.module';
-import { UsersController } from './modules/users/users.controller';
-import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './modules/prisma/prisma.module'
+import { ProjectsModule } from './modules/projects/projects.module'
+import { TasksModule } from './modules/tasks/tasks.module'
+import { UsersModule } from './modules/users/users.module'
 
 @Module({
   imports: [ProjectsModule, PrismaModule, TasksModule, UsersModule],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
