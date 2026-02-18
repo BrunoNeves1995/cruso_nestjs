a4523d0849f27354b6f75ab688d704f146cc968e
Bruno Neves <brunoneves814@gmail.com>
2026-02-17 21:04:28 -0400

    craate module collaborators

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './modules/prisma/prisma.module'
 import { ProjectsModule } from './modules/projects/projects.module'
 import { TasksModule } from './modules/tasks/tasks.module'
 import { UsersModule } from './modules/users/users.module'
+import { CollaboratorsModule } from './modules/collaborators/collaborators.module';
 
 @Module({
-  imports: [ProjectsModule, PrismaModule, TasksModule, UsersModule],
+  imports: [ProjectsModule, PrismaModule, TasksModule, UsersModule, CollaboratorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
